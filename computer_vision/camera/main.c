cf335264ac3ab7d12ccd2a169a5fd1219a89b191
clems4ever <clement.michaud34@gmail.com>
2013-10-20 23:25:21 +0200

    Adds an example of how to use the camera with opencv

---
+++ computer_vision/camera/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <iostream>
+#include "opencv2/core/core.hpp"
+#include "opencv2/nonfree/features2d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+using namespace cv;
+
+void readme();
+
+/** @function main */
+int main( int argc, char** argv )
+{
+  VideoCapture cap(1);
+  Mat camera_frame;
+
+
+
+
+  while(1)
+  {
+    cap.read(camera_frame);
+    imshow("Keypoints 1", camera_frame );
+    waitKey(30);
+  }
+
+  return 0;
+  }
+
+  /** @function readme */
+  void readme()
+  { std::cout << " Usage: ./SURF_detector <img1>" << std::endl; }
