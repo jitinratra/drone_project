1ae7e7e7f3c341f9c244da5fef1a089dfca90f1e
Jules Waldhart <waldhart@etud.insa-toulouse.fr>
2013-11-02 16:38:20 +0100

    GPS ok + starting puting it into API form.

--- src/gps_remote_driver.cpp
+++ src/gps_remote_driver.cpp
@@ -9,30 +9,31 @@
 #include <sstream>
 
 #include "ros/ros.h"
+#include "sensor_msgs/NavSatFix.h"
 
-void error(const char *msg)
-{
-	perror(msg);
-	exit(0);
-}
+//gps conversion tools
+#include "./gps_conv.h"
 
 
-void convert_and_pub(char *data,ros::Publisher<sensor_msgs::NavSatFix> &publisher){
+void convert_and_pub(char *data,ros::Publisher &publisher){
 	sensor_msgs::NavSatFix msgNav;
-	msgNav.header.stamp = ros::time();
+	msgNav.header.stamp = ros::Time::now();
 	msgNav.header.frame_id="/ardrone/gps_antenna";
+
+	degreesMinutesToDegrees(81.1);
+	GeoPoint position = analyseGpsGga(data);
 
 	//msgNav.status ?
 
 	// Latitude [degrees]. Positive is north of equator; negative is south.
-	msgNav.latitude;
+	msgNav.latitude = position.latitude;
 
 	// Longitude [degrees]. Positive is east of prime meridian; negative is west.
-	msgNav.longitude;
+	msgNav.longitude = position.longitude;
 
 	// Altitude [m]. Positive is above the WGS 84 ellipsoid
 	// (quiet NaN if no altitude is available).
-	msgNav.altitude;
+	//msgNav.altitude;
 
 	// Position covariance [m^2] defined relative to a tangential plane
 	// through the reported position. The components are East, North, and
@@ -48,17 +49,20 @@ void convert_and_pub(char *data,ros::Publisher<sensor_msgs::NavSatFix> &publishe
 	// estimate an approximate covariance from that.
 
 	msgNav.position_covariance_type=msgNav.COVARIANCE_TYPE_UNKNOWN;
+
+	publisher.publish(msgNav);
+	return;
 }
 
 
 int main(int argc, char *argv[])
 {
-	int sockfd, portno, n;
+	int sockfd, portno, n=0;
 	struct sockaddr_in serv_addr;
 	struct hostent *server;
 
 
-	ros::init(argc,argv,"testTCP");
+	ros::init(argc,argv,"gps_remote_driver");
 	ros::NodeHandle nh;
 
 	ros::Publisher navdataPub= nh.advertise<sensor_msgs::NavSatFix>("gps",10);
@@ -72,7 +76,7 @@ int main(int argc, char *argv[])
 	portno = atoi(argv[2]);
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd < 0) 
-		error("ERROR opening socket");
+		ROS_ERROR("ERROR opening socket");
 	server = gethostbyname(argv[1]);
 	if (server == NULL) {
 		ROS_ERROR("ERROR, no such host\n");
@@ -90,8 +94,6 @@ int main(int argc, char *argv[])
 	//bzero(buffer,256);
 	//fgets(buffer,255,stdin);
 	//n = write(sockfd,buffer,strlen(buffer));
-	if (n < 0) 
-		ROS_ERROR("ERROR writing to socket");
 
 	int i=0,i2=0;
 	while(ros::ok()){
@@ -100,17 +102,21 @@ int main(int argc, char *argv[])
 		if (n < 0) {
 			ROS_ERROR("ERROR reading from socket");
 			break;
+		}else if(n==0){
+			ROS_WARN("nothing received");
 		}
 		i=0;
-		ROS_DEBUG("\t\treceived %s",buffer);
-		while((buffer[i]!='\0') && (i<sizeof(buffer))){
+		ROS_INFO("\t\treceived %s",buffer);
+		while((buffer[i]!='\0') && (i<static_cast<int>(sizeof(buffer)))){
 			//put char in buffer2
 			buffer2[i2]=buffer[i];
-			if((buffer2[i2]=='\n') || (i2>=sizeof(buffer2)-1)){
+			if((buffer2[i2]=='\n') || (i2>=static_cast<int>(sizeof(buffer2))-1)){
 				//we received an entire NMEA frame, go converting it
 				buffer[i2+1]='\0';
 				ROS_INFO(buffer2);
-				convert_and_pub(buffer2,navdataPub);
+				if ( std::string(buffer2).find("$GPGGA") == 0){
+					convert_and_pub(buffer2,navdataPub);
+				}
 				bzero(buffer2,256*4);
 				i2=-1;
 			}
