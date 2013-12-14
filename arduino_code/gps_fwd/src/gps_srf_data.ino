138881458693c7cfbc121c67e3071cdf879b154d
Jules Waldhart <waldhart@etud.insa-toulouse.fr>
2013-12-14 18:36:09 +0100

    modified arduino code to use gps at 10Hz NOT tested

--- arduino_code/gps_fwd/src/gps_srf_data.ino
+++ arduino_code/gps_fwd/src/gps_srf_data.ino
@@ -1,6 +1,8 @@
 #include <SoftwareSerial.h>
 
-#define CONTROL_CHECKSUM
+//#define CONTROL_CHECKSUM
+#define GPS_10_HZ
+#define BYTE_TO_BYTE
 
 #define STR_LEN_MAX 100
 #define STR_BUFF_MAX 15
@@ -21,6 +23,7 @@ byte byteGPS = 0;
 
 char *last_header = "VTG";
 
+#ifdef CONTROL_CHECKSUM
 char chartoval(char c){
 	if((c >=48) && (c<=57)){
 		//it's a digit (0-9)
@@ -48,21 +51,33 @@ char checksum(char *s) {
 
 	return c;
 }
+#endif
 
 
 void setup(){
-	Serial.begin(9600);// ouvre le port série et règle le debit à 9600 bps
-	gps.begin(9600);	// pareil pour les ports digitaux
+	Serial.begin(115200);// ouvre le port série et règle le debit à 115200 bps
+	gps.begin(9600);	// open gps SoftwareSerial port at 9600 bauds (default)
 	pinMode(SoftrxPin, INPUT);
+#ifdef GPS_10_HZ
+	gps.println("$PMTK251,115200*11"); //ask GPS to comunicate at 115200 bauds
+	delay(200);
+	gps.begin(115200); //change Software Serial baudrate after some time
+	gps.println("$PMTK226,3,30*4"); //ask gps to update at 10Hz
+#endif //GPS_10_HZ
 }
 
 void loop(){
+#ifndef BYTE_TO_BYTE
 	static char str_buff[STR_BUFF_MAX][STR_LEN_MAX];
 	char buff;
 	static int i=0,char_i=0,str_i=0;
 	static int decompteOk=0;
+#endif
 
 	if(gps.available()!=0){
+#ifdef BYTE_TO_BYTE
+		Serial.write(gps.read());
+#else
 		str_buff[str_i][char_i]=gps.read();
 		if((str_buff[str_i][char_i++]=='\n')||(char_i>=STR_LEN_MAX-1)){
 			str_buff[str_i][char_i]=0;
@@ -78,8 +93,9 @@ void loop(){
 			if(checksum(str_buff[str_i])==0){
 				//Serial.println("ERRORCHECKSUM");
 				//do not transmit == do not increment str_i : will be overwritten or not transmitted
-			}else{
-#endif
+			}else
+#endif //CONTROL_CHECKSUM
+			{
 				str_i++;
 				if(str_i>=STR_BUFF_MAX){
 					str_i--;
@@ -94,6 +110,7 @@ void loop(){
 		}
 		str_i=0;
 		decompteOk=-1;
+#endif //BYTE_TO_BYTE
 	}
 
 }
